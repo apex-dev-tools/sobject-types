99c064cd4de5cf3a3f7b3bdc4e22439859b05540
Peter Wright <pwright@financialforce.com>
2023-01-24 00:51:55 +0000

    build: Update generator scripts and run through maven

--- src/test/scala/com/financialforce/tools/CopyrightWriter.scala
+++ src/test/scala/com/financialforce/tools/CopyrightWriter.scala
@@ -8,7 +8,7 @@ object CopyrightWriter {
 
     writer.write(
       """/*
-        | * Copyright (c) 2022 FinancialForce.com, inc. All rights reserved.
+        | * Copyright (c) 2023 FinancialForce.com, inc. All rights reserved.
         | */
         |""".stripMargin
     )
--- src/test/scala/com/financialforce/tools/SObjectToJava.scala
+++ src/test/scala/com/financialforce/tools/SObjectToJava.scala
@@ -2,41 +2,44 @@
 package com.financialforce.tools
 
 import java.io.{BufferedWriter, FileWriter}
-import java.nio.file.Paths
-
+import java.nio.file.{Files, Paths}
 
 object SObjectToJava {
-  def loginURL = "https://login.salesforce.com/services/Soap/u/56.0"
+  def loginURL(instance: String): String = s"https://$instance.salesforce.com/services/Soap/u/57.0"
 
   def main(args: Array[String]): Unit = {
 
     args.length match {
-      case 2 => ()
-      case _ => println("Usage: SObjectToJava <username> <password>"); return
+      case 3 => ()
+      case _ => println("Usage: SObjectToJava <username> <password> <instance>"); return
     }
 
-    val connectionResult = SFConnection.login(loginURL, args(0), args(1))
+    val connectionResult = SFConnection.login(loginURL(args(2)), args(0), args(1))
     if (connectionResult.isLeft) {
-      println(connectionResult.left.get)
+      println(connectionResult.swap.getOrElse("Connection failed"))
       return
     }
-    val partnerConnection = connectionResult.right.get.connection
+    val partnerConnection = connectionResult.toOption.get.connection
     println("Connected to Org")
     val sObjectNames = partnerConnection.describeGlobal().getSobjects.filterNot(
       dfr => dfr.getCustom || dfr.getCustomSetting
     ).map(_.getName)
 
+    var count = 0;
     sObjectNames.grouped(100).foreach(grp => {
       partnerConnection.describeSObjects(grp).foreach(sobjectDescribe => {
         val describedSObject = DescribedSObject(sobjectDescribe)
         if (describedSObject.fieldsAndTypes.exists(_._1 == "Id")) {
-          val output = Paths.get(s"src/test/scala/com/financialforce/tools/output/${describedSObject.name}.java").toFile
-          val writer = new BufferedWriter(new FileWriter(output))
+          val output = Paths.get(s"generated/${describedSObject.name}.java")
+          Files.createDirectories(output.getParent)
+          val writer = new BufferedWriter(new FileWriter(output.toFile))
           writer.write(describedSObject.asJava)
           writer.flush()
           writer.close()
+          count += 1;
         }
       })
     })
+    println(s"Generated $count files")
   }
 }
